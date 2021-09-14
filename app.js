7ac0422bfa20633d0ca43f3502e8074f3ca12595
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2021-09-14 23:27:19 +0530

    styling added, bonus part completed.

--- app.js
+++ app.js
@@ -1,30 +1,66 @@
+const billAmountDiv = document.querySelector(".bill-amt-div");
 const billAmount = document.querySelector("#bill-amount");
+const nextButton = document.querySelector("#btn-next");
+const cashGivenDiv = document.querySelector(".cash-given-div");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#btn-check");
 const errorMessage = document.querySelector("#error-message");
+const changeTable = document.querySelector(".change-table");
+const returnAmountDisplay = document.querySelector("#return-change-display-div");
+const returnAmountCaption = document.querySelector("#return-change-caption");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const denominations = [2000, 500, 100, 50, 10, 5, 1];
 
-function validateBillAndCashAmount() {
+cashGivenDiv.style.display = "none";
+checkButton.style.display = "none";
+changeTable.style.display = "none";
+errorMessage.style.display = "none";
+returnAmountDisplay.style.display = "none";
+returnAmountCaption.style.display = "none";
 
-    errorMessage.style.display = "none";
+function validateBillAmount() {
+
     if (billAmount.value > 0) {
-        // console.log(typeof cashGiven.value);
-        //  console.log(typeof billAmount.value);
-        //  console.log("Subtraction ",typeof(cashGiven.value - billAmount.value));
+        errorMessage.style.display = "none";
+        nextButton.style.display = "none";
+        cashGivenDiv.style.display = "block";
+        checkButton.style.display = "inline";
 
-        if (Number(cashGiven.value) >= Number(billAmount.value)) {
+    } else {
+        showErrorMessage("Enter a valid Bill Amount.");
+    }
+}
+
+function validateCashAmount() {
+
+    if (cashGiven.value > 0) {
+
+        errorMessage.style.display = "none";
+
+        if (Number(cashGiven.value) > Number(billAmount.value)) {
 
             const amountToBeReturned = cashGiven.value - billAmount.value;
-            console.log(amountToBeReturned);
+            returnAmountDisplay.style.display = "block";
+            returnAmountCaption.style.display = "block";
+            returnAmountDisplay.style.color = "green";
+            returnAmountDisplay.innerText ="₹ "+ amountToBeReturned;
+            changeTable.style.display = "block";
             calculateChange(amountToBeReturned);
-        } else {
-            showErrorMessage("Seems like you wanna wash plates!");
 
+        } 
+        else if(Number(cashGiven.value) === Number(billAmount.value)){
+            errorMessage.style.color = "green";
+            errorMessage.style.fontSize = "larger";
+            showErrorMessage("You are all clear! No cash to return.");
+        }
+        else {
+            errorMessage.style.color = "red";
+            errorMessage.style.fontSize = "larger";
+            showErrorMessage("Please give us more cash.");
         }
     } else {
-        showErrorMessage("Invalid Bill Amount");
+        showErrorMessage("Enter a valid Cash Amount.");
     }
 }
 
@@ -42,4 +78,5 @@ function showErrorMessage(message) {
     errorMessage.style.display = "block";
     errorMessage.innerText = message;
 }
-checkButton.addEventListener("click", validateBillAndCashAmount)
+nextButton.addEventListener("click", validateBillAmount);
+checkButton.addEventListener("click", validateCashAmount);
